78eb725bf2fc9c48d0764b8cdc3856e400efafcb
Replit user <>
2022-08-21 15:39:12 +0000

    new try

--- list.c
+++ list.c
@@ -45,7 +45,7 @@ void * firstList(List * list) {
 
 void * nextList(List * list) {
   if (!list->current->next) return NULL;
-  list->current = list->current->next;
+  list->current = list->current->prev;
   return (list->current->next);
 }
 
